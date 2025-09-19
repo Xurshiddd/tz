d95f0121ab31d88343607a9f8e6526cdc0facbc6
Xurshiddd <marifovxurshid800@gmailcom>
2025-09-19 10:26:19 +0500

    db commit

---
+++ frontend/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=host_tz',
+    'username' => 'host_host',
+    'password' => '#[m(yEpR6l]nN-o%',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+];
